1bf0f03875c6f01d740a5a4c19cd072594333a39
西故 <3099154616@qq.com>
2025-03-22 19:52:48 +0800

    添加多个示例程序以演示结构体使用和字符串操作

--- C++/C++ Primer Plus/No.4/4.9/4.9.cpp
+++ C++/C++ Primer Plus/No.4/4.9/4.9.cpp
@@ -1,14 +1,25 @@
-//strtype2.cpp -- assigning, adding,and appending
+//strtype3.cpp -- more string class features
 
 #include<iostream>
 #include<string>
+#include<cstring>
 int main()
 {
     using namespace std;
-    string s1 = "penguin";
-    string s2, s3;
-    cout << "You can assign one string object to another: s2 = s1\n";
-    s2 = s1;
-    cout << "s1: " << s1 << ", s2: " << s2 << endl;
-    
+    char charr1[20];
+    char charr2[20] = "jaguar";
+    string str1;
+    string str2 = "panther";
+    str1 = str2;
+    strcpy(charr1, charr2);
+    str1 += " paste";
+    strcat(charr1, " juice");
+    int len1 = str1.size();
+    int len2 = strlen(charr1);
+    cout << "The string " << str1 << " contains "
+         << len1 << " characters.\n";
+    cout << "The string " << charr1 << " contains "
+         << len2 << " characters.\n";
+    return 0;
 }
+
