de7b38a715039d65d707774c8d4828b6325557d9
西故 <3099154616@qq.com>
2025-03-22 19:02:37 +0800

    添加多个示例程序以演示字符串操作和数字输入

---
+++ C++/C++ Primer Plus/No.4/4.9/4.9.cpp
@@ -0,0 +1,14 @@
+//strtype2.cpp -- assigning, adding,and appending
+
+#include<iostream>
+#include<string>
+int main()
+{
+    using namespace std;
+    string s1 = "penguin";
+    string s2, s3;
+    cout << "You can assign one string object to another: s2 = s1\n";
+    s2 = s1;
+    cout << "s1: " << s1 << ", s2: " << s2 << endl;
+    
+}
