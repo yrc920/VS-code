e1f3c0256970783cf7d397e8a47ab68b2355d024
西故 <3099154616@qq.com>
2025-04-17 16:17:05 +0800

    添加 11.1.cpp 的初始实现，包含基本的算术运算示例

---
+++ C++/C++ Primer Plus/No.11/11.1/11.1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 10;
+    int c = 15;
+    int d = 20;
+
+    cout << "a + b = " << a + b << endl;
+    cout << "c - d = " << c - d << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "d / c = " << d / c << endl;
+
+    return 0;
+}
